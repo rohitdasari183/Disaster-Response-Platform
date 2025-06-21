35932c70ed2b7955faf8202cbbdeae15d3a7d4f3
rohitdasari183 <111290301+rohitdasari183@users.noreply.github.com>
2025-06-21 14:06:17 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://disaster-response-platform-1.onrender.com',
+  baseURL: 'https://disaster-response-platform-2.onrender.com',
 });
 
 export default API;
