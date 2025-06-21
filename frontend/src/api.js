e2ca79a98171860434a2c39195b5f38a267cebea
rohitdasari183 <111290301+rohitdasari183@users.noreply.github.com>
2025-06-21 13:05:18 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://disaster-response-platform-plui.vercel.app
+',
 });
 
 export default API;
