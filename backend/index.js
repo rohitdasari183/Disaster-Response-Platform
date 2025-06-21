eaeaffdfeae69d4bb171379557f31f0cb275240c
rohitdasari183 <111290301+rohitdasari183@users.noreply.github.com>
2025-06-21 13:18:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
 
 app.use(cors(
   {
-    origin:[""],
+    origin:["disaster-response-platform-front.vercel.app"],
     methods:["POST","GET"],
     credentials:true
   }
